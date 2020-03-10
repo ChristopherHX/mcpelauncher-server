3d8b302c914358a4d5b7d13ab1a6885c79f05358
Christopher Homberger <christopher.homberger@web.de>
2020-03-10 16:28:10 +0100

    Warning that only 1.12 is supported remove require OPENSSL_armcap=0 on armhf

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,11 @@ int main(int argc, char *argv[]) {
         MinecraftUtils::setMallocZero();
 
     MinecraftUtils::setupForHeadless();
+    Log::Warn("Launcher", "Only Minecraft 1.12.0.28 and 1.12.1.1 are supported");
+#ifdef __arm__
+    // Get rid of defining OPENSSL_armcap
+    hybris_hook("OPENSSL_cpuid_setup", (void*) + []() -> void {});
+#endif
 
     Log::trace("Launcher", "Loading Minecraft library");
     void* handle = MinecraftUtils::loadMinecraftLib();
